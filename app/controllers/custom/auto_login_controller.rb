72eafd6c5faf7fea8d5e292d3dbc2fca3831c039
Amaia Castro <amaia@amaiac.net>
2017-06-09 11:32:00 +0200

    Only allow auto login when feature enabled

--- app/controllers/custom/auto_login_controller.rb
+++ app/controllers/custom/auto_login_controller.rb
@@ -2,6 +2,8 @@ class AutoLoginController < ApplicationController
   skip_authorization_check
 
   def index
+    render(file: "#{Rails.root}/public/404.html", layout: false, status: 404) and return unless Setting["feature.auto_login"]
+
     base_64_nif = params[:NIF]
 
     if nif = Base64.strict_decode64(base_64_nif) rescue nil
--- spec/features/autologin_spec.rb
+++ spec/features/autologin_spec.rb
@@ -3,10 +3,21 @@ require 'rails_helper'
 feature 'Autologin' do
 
   background do
+    Setting["feature.auto_login"] = true
+
     @nif = '12345678N'
     @encoded_nif = Base64.strict_encode64(@nif)
     @user = create(:user, username: "Manuela Colau", document_number: @nif)
   end
+
+  scenario 'Autologin feature disabled' do
+    Setting["feature.auto_login"] = false
+
+    visit "/Pedidos/?NIF=#{@encoded_nif}"
+
+    expect(page.status_code).to eq(404)
+  end
+
 
   scenario 'Autologin feature with valid NIF' do
     visit "/Pedidos/?NIF=#{@encoded_nif}"
