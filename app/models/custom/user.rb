e21706d18cc1a7503264cc65a68e0ba37fa9dafa
Amaia Castro <amaia@amaiac.net>
2017-06-09 11:56:46 +0200

    Refactor to avoid crashes by accessing Setting before the table exists when running db:setup or db:migrate

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -1,9 +1,11 @@
 require_dependency Rails.root.join('app', 'models', 'user').to_s
 
 class User
-  before_create :set_verified if Setting["feature.#{:auto_verify_users}"]
+  before_create :set_verified
 
   def set_verified
+    return unless Setting["feature.#{:auto_verify_users}"]
+
     self.verified_at = Time.current
     self.residence_verified_at = Time.current
   end
