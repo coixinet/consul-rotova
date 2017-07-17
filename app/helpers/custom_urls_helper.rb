5f429181e93b48f97d3d8655ccd911ba1a3851ff
Fernando Blat <ferblape@gmail.com>
2017-07-17 15:53:33 +0200

    Custom paths

--- app/helpers/custom_urls_helper.rb
+++ app/helpers/custom_urls_helper.rb
@@ -6,4 +6,12 @@ module CustomUrlsHelper
   def legislation_annotation_url(annotation)
     legislation_process_question_url(annotation.draft_version.process, annotation.draft_version, annotation)
   end
+
+  def legislation_question_path(question)
+    legislation_process_question_path(question.process, question)
+  end
+
+  def legislation_annotation_path(annotation)
+    legislation_process_question_path(annotation.draft_version.process, annotation.draft_version, annotation)
+  end
 end
