rockandror/consuldemocracy

Add public interests validation and show interests on user profile

app/controllers/account_controller.rb
@@ -25,7 +25,7 @@ class AccountController < ApplicationController
       if @account.organization?
         params.require(:account).permit(:phone_number, :email_on_comment, :email_on_comment_reply, :newsletter, organization_attributes: [:name, :responsible_name])
       else
-        params.require(:account).permit(:username, :public_activity, :email_on_comment, :email_on_comment_reply, :email_on_direct_message, :email_digest, :newsletter, :official_position_badge)
+        params.require(:account).permit(:username, :public_activity, :public_interest, :email_on_comment, :email_on_comment_reply, :email_on_direct_message, :email_digest, :newsletter, :official_position_badge)
       end
     end
 

app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     load_filtered_activity if valid_access?
+    load_interest if valid_interest_access?
   end
 
   private
@@ -62,8 +63,16 @@ class UsersController < ApplicationController
       @budget_investments = Budget::Investment.where(author_id: @user.id).order(created_at: :desc).page(params[:page])
     end
 
+    def load_interest
+      @interests = Follow.interests_by(@user)
+    end
+
     def valid_access?
       @user.public_activity || authorized_current_user?
+    end
+
+    def valid_interest_access?
+      @user.public_interest || authorized_current_user?
     end
 
     def author?

spec/features/users_spec.rb
@@ -213,6 +213,88 @@ feature 'Users' do
 
   end
 
+  feature 'Public interest' do
+    background do
+      @user = create(:user)
+    end
+
+    scenario 'show interests' do
+      proposal =  create(:proposal, tag_list: "Sport")
+      create(:follow, :followed_proposal, followable: proposal, user: @user)
+
+      login_as(@user)
+      visit account_path
+
+      check 'account_public_interest'
+      click_button 'Save changes'
+
+      logout
+
+      visit user_path(@user)
+      expect(page).to have_content("Sport")
+    end
+
+    scenario 'no visible by default' do
+      visit user_path(@user)
+
+      expect(page).to have_content(@user.username)
+      expect(page).not_to have_css('#public_interest')
+    end
+
+    scenario 'user can show public page' do
+      login_as(@user)
+      visit account_path
+
+      check 'account_public_interest'
+      click_button 'Save changes'
+
+      logout
+
+      visit user_path(@user)
+      expect(page).to have_css('#public_interest')
+    end
+
+    scenario 'is always visible for the owner' do
+      login_as(@user)
+      visit account_path
+
+      uncheck 'account_public_interest'
+      click_button 'Save changes'
+
+      visit user_path(@user)
+      expect(page).to have_css('#public_interest')
+    end
+
+    scenario 'is always visible for admins' do
+      login_as(@user)
+      visit account_path
+
+      uncheck 'account_public_interest'
+      click_button 'Save changes'
+
+      logout
+
+      login_as(create(:administrator).user)
+      visit user_path(@user)
+      expect(page).to have_css('#public_interest')
+    end
+
+    scenario 'is always visible for moderators' do
+      login_as(@user)
+      visit account_path
+
+      uncheck 'account_public_interest'
+      click_button 'Save changes'
+
+      logout
+
+      login_as(create(:moderator).user)
+      visit user_path(@user)
+      expect(page).to have_css('#public_interest')
+    end
+
+  end
+
   feature 'Special comments' do
 
     scenario 'comments posted as moderator are not visible in user activity' do

coixinet/consul-rotova

app/controllers/account_controller.rb
@@ -25,7 +25,7 @@ class AccountController < ApplicationController
       if @account.organization?
         params.require(:account).permit(:phone_number, :email_on_comment, :email_on_comment_reply, :newsletter, organization_attributes: [:name, :responsible_name])
       else
-        params.require(:account).permit(:username, :public_activity, :email_on_comment, :email_on_comment_reply, :email_on_direct_message, :email_digest, :newsletter, :official_position_badge)
+        params.require(:account).permit(:username, :public_activity, :public_interests, :email_on_comment, :email_on_comment_reply, :email_on_direct_message, :email_digest, :newsletter, :official_position_badge)
       end
     end
 

app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     load_filtered_activity if valid_access?
+    load_interest if valid_interest_access?
   end
 
   private
@@ -62,8 +63,16 @@ class UsersController < ApplicationController
       @budget_investments = Budget::Investment.where(author_id: @user.id).order(created_at: :desc).page(params[:page])
     end
 
+    def load_interest
+      @interests = Follow.interests_by(@user)
+    end
+
     def valid_access?
       @user.public_activity || authorized_current_user?
+    end
+
+    def valid_interest_access?
+      @user.public_interests || authorized_current_user?
     end
 
     def author?

spec/features/users_spec.rb
@@ -213,6 +213,88 @@ feature 'Users' do
 
   end
 
+  feature 'Public interest' do
+    background do
+      @user = create(:user)
+    end
+
+    scenario 'show interests' do
+      proposal =  create(:proposal, tag_list: "Sport")
+      create(:follow, :followed_proposal, followable: proposal, user: @user)
+
+      login_as(@user)
+      visit account_path
+
+      check 'account_public_interests'
+      click_button 'Save changes'
+
+      logout
+
+      visit user_path(@user)
+      expect(page).to have_content("Sport")
+    end
+
+    scenario 'no visible by default' do
+      visit user_path(@user)
+
+      expect(page).to have_content(@user.username)
+      expect(page).not_to have_css('#public_interests')
+    end
+
+    scenario 'user can show public page' do
+      login_as(@user)
+      visit account_path
+
+      check 'account_public_interests'
+      click_button 'Save changes'
+
+      logout
+
+      visit user_path(@user)
+      expect(page).to have_css('#public_interests')
+    end
+
+    scenario 'is always visible for the owner' do
+      login_as(@user)
+      visit account_path
+
+      uncheck 'account_public_interests'
+      click_button 'Save changes'
+
+      visit user_path(@user)
+      expect(page).to have_css('#public_interests')
+    end
+
+    scenario 'is always visible for admins' do
+      login_as(@user)
+      visit account_path
+
+      uncheck 'account_public_interests'
+      click_button 'Save changes'
+
+      logout
+
+      login_as(create(:administrator).user)
+      visit user_path(@user)
+      expect(page).to have_css('#public_interests')
+    end
+
+    scenario 'is always visible for moderators' do
+      login_as(@user)
+      visit account_path
+
+      uncheck 'account_public_interests'
+      click_button 'Save changes'
+
+      logout
+
+      login_as(create(:moderator).user)
+      visit user_path(@user)
+      expect(page).to have_css('#public_interests')
+    end
+
+  end
+
   feature 'Special comments' do
 
     scenario 'comments posted as moderator are not visible in user activity' do