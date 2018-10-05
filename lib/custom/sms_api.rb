fe96f83a2b4828d8826bfc422c2f8604ef9ea946
Alberto Miedes <albertomg994@gmail.com>
2018-10-05 12:08:07 +0200

    Integrate SMS

--- lib/custom/sms_api.rb
+++ lib/custom/sms_api.rb
@@ -1,56 +1,82 @@
-require "uri"
-require "net/http"
-require "json"
+require 'open-uri'
 
 class SMSApi
 
+  attr_accessor :client
+
+  def initialize
+    @client = Savon.client(wsdl: wsdl_path, log: true)
+  end
+
+  def wsdl_path
+    Rails.root.join('config', 'sms_wsdl.xml')
+  end
+
   def sms_deliver(phone, code)
-    return true unless end_point_available?
+    Rails.logger.info("Verification code: #{code}") if Rails.env.development?
 
-    request = Net::HTTP::Post.new(request_url)
-    request["content-type"] = "application/json"
-    request["accept"] = "application/json"
-    request.body = request_body(phone, code)
+    response_body = if end_point_available?
+                      client.call(
+                        :sms_text_submit,
+                        message: build_message(phone, code)
+                      ).body
+                    else
+                      stubbed_response_body
+                    end
 
-    response = http_client.request(request)
-    response.read_body
-    return true
+    success?(response_body)
   end
 
-  private
-
-  def request_body(to, code)
+  def build_message(phone, code)
     {
-      api_key: Rails.application.secrets.sms_password,
-      concat: 1,
-      messages: [
-        {
-          from: "Gob.Abierto",
-          to: to =~ /\A\+34/ ? to : "+34#{to}",
-          text: "Clave para verificarte: #{code}. Gobierno Abierto"
-        }
-      ]
-    }.to_json
-  end
-
-  def http_client
-    @http_client ||= setup_http_client
-  end
-
-  def request_url
-    @request_url ||= URI(Rails.application.secrets.sms_end_point)
-  end
-
-  def setup_http_client
-    http_client = Net::HTTP.new(request_url.host, request_url.port)
-    http_client.use_ssl = true
-    http_client.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    http_client
+      'Version' => '1.0',
+      'Authorization' => authorization,
+      'Sender' => sender_phone,
+      'Recipients' => { 'To' => [phone] },
+      'SMSText' => "Clave para verificarte: #{code}. Gobierno Abierto",
+      'SMSClass' => '1'
+    }
   end
 
   def end_point_available?
     Rails.env.staging? || Rails.env.preproduction? || Rails.env.production?
+  end
+
+  private
+
+  def success?(response_body)
+    response_body.dig(:submit_res, :status, :status_text) == 'Success'
+  end
+
+  def authorization
+    Base64.strict_encode64("#{service_user}:#{service_password}")
+  end
+
+  def service_user
+    Rails.application.secrets.sms_username
+  end
+
+  def service_password
+    Rails.application.secrets.sms_password
+  end
+
+  def sender_phone
+    Rails.application.secrets.sms_sender_phone
+  end
+
+  def stubbed_response_body
+    {
+      submit_res: {
+        version: '1.0',
+        status: {
+          status_code: '1000',
+          status_text: 'Success',
+          details: 'Request processed succesfully'
+        },
+        message_id: 'example-123',
+        '@xmlns:ns1': 'http://www.example.com/schemas'
+      }
+    }
   end
 
 end
