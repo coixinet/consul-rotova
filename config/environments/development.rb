c1db2402139294909cf693ea8fabb6355bc8239c
Josep Vicent Monjo i Agut <josep@MacBook-Air-de-Josep.local>
2019-11-07 15:24:48 +0100

    CHANGED: mail server in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,9 +14,9 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.asset_host = "http://localhost:3000"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
+  config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
 
   # Deliver emails to a development mailbox at /letter_opener
   config.action_mailer.delivery_method = :letter_opener
