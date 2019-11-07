f42b94a8e6aec6c0af228ae900c2db894cf64ef8
Josep Vicent Monjo i Agut <josep@MacBook-Air-de-Josep.local>
2019-11-07 16:30:47 +0100

    HARDCODED url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
-  config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
+  config.action_mailer.default_url_options = { host: 'participacio.rotova.es' }
+  config.action_mailer.asset_host = "https://participacio.rotova.es"
 
   # Deliver emails to a development mailbox at /letter_opener
   config.action_mailer.delivery_method = :letter_opener
