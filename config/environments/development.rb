0607ce7ddd5468c46e037680230f4a05a942f226
Josep Vicent Monjo i Agut <josep@MacBook-Air-de-Josep.local>
2019-11-09 13:44:39 +0100

    CHANGED: hardcoded to env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'participacio.rotova.es' }
-  config.action_mailer.asset_host = "https://participacio.rotova.es"
+  config.action_mailer.default_url_options = { host: ENV["MAIN_URL"] }
+  config.action_mailer.asset_host = ENV["MAIN_URL"]
 
   # Deliver emails to a development mailbox at /letter_opener
   config.action_mailer.delivery_method = :letter_opener
