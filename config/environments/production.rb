bd5e7f7ec890bd1d0d5455312546f94177280770
Josep Vicent Monjo i Agut <josep@MacBook-Air-de-Josep.local>
2019-11-09 14:27:02 +0100

    ADDED: mail config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,21 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
-  config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
+  config.action_mailer.default_url_options = { host: ENV["MAIN_URL"] }
+  config.action_mailer.asset_host = ENV["MAIN_URL"]
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      :address              => ENV["MAIL_HOST"],
+      :port                 => ENV["MAIL_PORT"],
+      :domain               => ENV["MAIL_USER"],
+      :user_name            => ENV["MAIL_USER"],
+      :password             => ENV["MAIL_PASSWORD"],
+      :authentication => :plain,
+      :enable_starttls_auto => true,
+      :ssl => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
