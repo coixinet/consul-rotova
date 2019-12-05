0a08ac2f412de57ec002e374724382a5c36b996d
Josep <jvmonjo@gmail.com>
2019-12-05 14:00:37 +0100

    Update application_custom.rb

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -7,5 +7,21 @@ module Consul
     require Rails.root.join('lib/document_parser')
     require Rails.root.join('lib/custom/census_api')
     require Rails.root.join('lib/custom/sms_api')
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_url_options = { host: ENV["MAIN_URL"] }
+    config.action_mailer.asset_host = ENV["MAIN_URL"]
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+
+    ActionMailer::Base.smtp_settings = {
+      :user_name => ENV["MAIL_USER"],
+      :password => ENV["MAIL_PASSWORD"],
+      :domain => ENV["MAIL_DOMAIN"],
+      :address => ENV["MAIL_HOST"],
+      :port => ENV["MAIL_PORT"],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
   end
 end
