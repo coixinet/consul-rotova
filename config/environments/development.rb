0dd777c99c0fd8635599a2033101cfebf9c19f81
Josep <jvmonjo@gmail.com>
2019-12-05 13:15:06 +0100

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,13 +49,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-      :address              => ENV["MAIL_HOST"],
-      :port                 => ENV["MAIL_PORT"],
-      :domain               => ENV["MAIL_DOMAIN"],
-      :user_name            => ENV["MAIL_USER"],
-      :password             => ENV["MAIL_PASSWORD"],
-      :authentication => :plain,
-      :enable_starttls_auto => true,
+      :address              => "srv-captain--smtp",
+      :port                 => 25,
       # :ssl => true
   }
 
