5e67b414d3e5ac01cd8b58feaceb4095881f66e2
monjo <jvmonjo@gmail.com>
2020-01-22 13:34:55 +0100

    ACTIONMAILER::Base instead of config.action_mailer

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -7,16 +7,24 @@ module Consul
     require Rails.root.join('lib/document_parser')
     require Rails.root.join('lib/custom/census_api')
     require Rails.root.join('lib/custom/sms_api')
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.default_url_options = { host: ENV["MAIN_URL"] }
-    config.action_mailer.asset_host = ENV["MAIN_URL"]
+    # config.action_mailer.raise_delivery_errors = true
+    # config.action_mailer.default_url_options = { host: ENV["MAIN_URL"] }
+    # config.action_mailer.asset_host = ENV["MAIN_URL"]
+
+    ActionMailer::Base.raise_delivery_errors = true
+    ActionMailer::Base.default_url_options = { host: ENV["MAIN_URL"] }
+    ActionMailer::Base.asset_host = ENV["MAIN_URL"]
     
     class Application < Rails::Application
       config.web_console.whitelisted_ips = %w(127.0.0.0 47.61.87.94 10.0.0.0/16)
     end
 
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
+    # config.action_mailer.delivery_method = :smtp
+    # config.action_mailer.perform_deliveries = true
+
+    ActionMailer::Base.delivery_method = :smtp
+
+    ActionMailer::Base.perform_deliveries = true
 
     ActionMailer::Base.smtp_settings = {
       :user_name => ENV["MAIL_USER"],
