01dbb08dae8c5407aab02537eda08eaf24ea3d58
monjo <jvmonjo@gmail.com>
2020-01-24 10:54:56 +0100

    test disable tls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
     :address => ENV["MAIL_HOST"],
     :port => ENV["MAIL_PORT"],
     :authentication => :plain,
-    :enable_starttls_auto => true,
+    :enable_starttls_auto => false,
     :ssl => true,
     :tls => false,
   }
